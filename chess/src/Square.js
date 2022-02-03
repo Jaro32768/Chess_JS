96be8af3a77c0797fcc3499bd1d5c10ca2d65020
Jaro <jarko04@hotmail.sk>
2022-02-03 13:25:18 +0100

    Small change

--- chess/src/Square.js
+++ chess/src/Square.js
@@ -4,13 +4,11 @@ import './styles.css';
 export default function Square(props) {
     const [className, setClassName] = useState(null);
 
-    if (className == null)
+    if (className === null)
         if ((props.row + props.column) % 2 === 0) setClassName('lightSquare')
         else setClassName('darkSquare')
 
     return (
-        <>
         <div className={className}></div>
-        </>
     );
 }
