978aa10bd3fa62c36b5cbecfebfb06889cd53a47
Jaro <jarko04@hotmail.sk>
2022-02-18 17:12:02 +0100

    Fix highlighting

--- chess/src/Board.js
+++ chess/src/Board.js
@@ -96,7 +96,7 @@ export default function Board() {
     const setLegalMoves = () => {
         resetLegalMoves();                  // resets legal moves
         for (let i = 0; i < 64; i++) {      // for each square on board
-            if (i % 5 === 0 ) {             // JUST PLACEHOLDER FOR NOW, SHOULD BE CHANGED TO CALL OF getLegalMoves() WHICH WILL BE COMPARED WITH INDEX
+            if (false) {                    // JUST PLACEHOLDER FOR NOW, SHOULD BE CHANGED TO CALL OF getLegalMoves() WHICH WILL BE COMPARED WITH INDEX
                 pieces[i][1] = true;        // sets move to legal
                 areLegalMovesSet = true;    // avoids infinite loop
             }
@@ -113,9 +113,9 @@ export default function Board() {
     
     // makes move //
     const handleClick = (senderRow, senderColumn) => {
-        if (pieces[8 * selectedRow + selectedColumn][0] === null ||                                 // if clicked square is empty
+        if (selectedRow === null && selectedColumn === null && pieces[8 * senderRow + senderColumn][0] === null) return;    // if there is not any selected square now and before, it does nothing
+        else if (pieces[8 * selectedRow + selectedColumn][0] === null ||                            // if clicked square is empty
             selectedRow === null) {                                                                 // or if this is first clicked square
-
             highlightedSquare = 8 * senderRow + senderColumn                                        // highlights selected square
             setSelectedRow(senderRow);                                                              // remembers row of clicked square
             setSelectedColumn(senderColumn);                                                        // remembers column of clicked square
@@ -123,12 +123,14 @@ export default function Board() {
         else if (selectedRow === senderRow && selectedColumn === senderColumn) {                    // if clicked square is same as previously clicked square - deselect            
             setSelectedRow(null);                                                                   // clears previously selected row
             setSelectedColumn(null);                                                                // clears previously selected column
+            highlightedSquare = null;                                                               // sets highlighted square to none
         }
         else {
             pieces[8 * senderRow + senderColumn][0] = pieces[8 * selectedRow + selectedColumn][0];  // copies piece from previously clicked square to clicked square
             pieces[8 * selectedRow + selectedColumn][0] = null;                                     // removes piece from previously clicked square
             setSelectedRow(null);                                                                   // clears previously selected row
             setSelectedColumn(null);                                                                // clears previously selected column
+            highlightedSquare = null;                                                               // sets highlighted square to none
         }
     }
 
