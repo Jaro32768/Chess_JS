74e87f199d79fa7be15aa26878d69b9689afe17e
Jaro <jarko04@hotmail.sk>
2022-02-25 15:48:36 +0100

    Implement king movement

--- react/src/pieces/King.js
+++ react/src/pieces/King.js
@@ -1,5 +1,24 @@
 export function getLegalMovesK(position, aRookMoved, hRookMoved, kingMoved, kingAttacked) {
+    // this array is showing if it is allowed to go there //
+    let isLegal = [false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false];
 
+    if (position % 8 > 0 && position / 8 > 0) isLegal[position - 9] = true;
+    if (position / 8 > 0) isLegal[position - 8] = true;
+    if (position % 8 < 7 && position / 8 > 0) isLegal[position - 7] = true;
+    if (position % 8 < 7) isLegal[position + 1] = true;
+    if (position % 8 < 7 && position / 8 < 7) isLegal[position + 9] = true;
+    if (position / 8 < 7) isLegal[position + 8] = true;
+    if (position % 8 > 0 && position / 8 < 7) isLegal[position + 7] = true;
+    if (position % 8 > 0) isLegal[position - 1] = true;
+
+    return isLegal;
 }
 
 function longCastle() {
