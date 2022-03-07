2d02b0a95c52e4c7d3832e4f89af20d5b9e72da7
Jaro <jarko04@hotmail.sk>
2022-03-07 17:08:00 +0100

    Add FEN (import, export)

--- react/src/Functions.js
+++ react/src/Functions.js
@@ -17,3 +17,11 @@ export function positionIncludesBothAndBothFirst(board, position1, position2) { 
 function positionIncludesWhiteAndNot(board, position1, position2) { return positionIncludesWhite(board, position1) && !positionIncludesWhite(board, position2) }
 function positionIncludesBlackAndNot(board, position1, position2) { return positionIncludesBlack(board, position1) && !positionIncludesBlack(board, position2) }
 export function positionIncludesBothAndNot(board, position1, position2) { return  positionIncludesBlackAndNot(board, position1, position2) || positionIncludesWhiteAndNot(board, position1, position2) }
+export function pieceLetterToWord(letter) { switch (letter) {
+        case 'r': return 'black-rook'; case 'n': return 'black-knight'; case 'b': return 'black-bishop'; case 'k': return 'black-king'; case 'q': return 'black-queen'; case 'p': return 'black-pawn';
+        case 'R': return 'white-rook'; case 'N': return 'white-knight'; case 'B': return 'white-bishop'; case 'K': return 'white-king'; case 'Q': return 'white-queen'; case 'P': return 'white-pawn';
+        case ' ': return null;}}
+export function pieceWordToLetter(word) { switch(word) {
+        case 'black-rook': return 'r'; case 'black-knight': return 'n'; case 'black-bishop': return 'b'; case 'black-king': return 'k'; case 'black-queen': return 'q'; case 'black-pawn': return 'p';
+        case 'white-rook': return 'R'; case 'white-knight': return 'N'; case 'white-bishop': return 'B'; case 'white-king': return 'K'; case 'white-queen': return 'Q'; case 'white-pawn': return 'P';
+        case null: return ' ';}}
--- react/src/components/Board.js
+++ react/src/components/Board.js
@@ -1,101 +1,24 @@
-import React, {useState} from 'react';
-import Square from './Square.js';                       // imports Square component
-import {getLegalMovesP} from '../pieces/Pawn.js';       // imports method that searches for legal moves for pawn
-import {getLegalMovesR} from '../pieces/Rook.js';       // imports method that searches for legal moves for rook
-import {getLegalMovesN} from '../pieces/Knight.js';     // imports method that searches for legal moves for knight
-import {getLegalMovesB} from '../pieces/Bishop.js';     // imports method that searches for legal moves for bishop
-import {getLegalMovesQ} from '../pieces/Queen.js';      // imports method that searches for legal moves for queen
-import {getLegalMovesK} from '../pieces/King.js';       // imports method that searches for legal moves for king
-import {rowAndColumnToPosition} from '../Functions.js'  // imports converting method
+import React, { useState } from 'react';
+import Square from './Square.js';                                                                                               // imports Square component
+import { getLegalMovesP } from '../pieces/Pawn.js';                                                                             // imports method that searches for legal moves for pawn
+import { getLegalMovesR } from '../pieces/Rook.js';                                                                             // imports method that searches for legal moves for rook
+import { getLegalMovesN } from '../pieces/Knight.js';                                                                           // imports method that searches for legal moves for knight
+import { getLegalMovesB } from '../pieces/Bishop.js';                                                                           // imports method that searches for legal moves for bishop
+import { getLegalMovesQ } from '../pieces/Queen.js';                                                                            // imports method that searches for legal moves for queen
+import { getLegalMovesK } from '../pieces/King.js';                                                                             // imports method that searches for legal moves for king
+import { rowAndColumnToPosition, pieceLetterToWord, pieceWordToLetter, positionToRow, positionToColumn} from '../Functions.js'  // imports converting methods
 
 let highlightedSquare = null,                           // remembers which square should be highlighted
     whiteKingMoved = false, blackKingMoved = false,     // remembers if kings moved
     whiteARookMoved = false, blackARookMoved = false,   // remembers if a rooks moved
     whiteHRookMoved = false, blackHRookMoved = false,   // remembers if h rooks moved
     isWhitesMove = true,                                // remembers whose move it is
-    startIndex, endIndex;                               // remembers start and end index for rendering squares
+    startIndex, endIndex,                               // remembers start and end index for rendering squares
+    halfmoveClock = 0,                                  // counting for 50 move == draw rule
+    fullmoveCounter = 1;                                // remmebers how many moves were played
 
-const pieces = [                        // content of each position on board - [ pieceOnSquare , isPossibleToMoveThere ]
-    // 8TH RANK //
-    [ 'black-rook', false ],            // notation: a8     row: 0     column: 0
-    [ 'black-knight', false ],          // notation: b8     row: 0     column: 1
-    [ 'black-bishop', false ],          // notation: c8     row: 0     column: 2
-    [ 'black-queen', false ],           // notation: d8     row: 0     column: 3
-    [ 'black-king', false ],            // notation: e8     row: 0     column: 4
-    [ 'black-bishop', false ],          // notation: f8     row: 0     column: 5
-    [ 'black-knight', false ],          // notation: g8     row: 0     column: 6
-    [ 'black-rook', false ],            // notation: h8     row: 0     column: 7
-
-    // 7TH RANK //
-    [ 'black-pawn', false ],            // notation: a7     row: 1     column: 0
-    [ 'black-pawn', false ],            // notation: b7     row: 1     column: 1
-    [ 'black-pawn', false ],            // notation: c7     row: 1     column: 2
-    [ 'black-pawn', false ],            // notation: d7     row: 1     column: 3
-    [ 'black-pawn', false ],            // notation: e7     row: 1     column: 4
-    [ 'black-pawn', false ],            // notation: f7     row: 1     column: 5
-    [ 'black-pawn', false ],            // notation: g7     row: 1     column: 6
-    [ 'black-pawn', false ],            // notation: h7     row: 1     column: 7
-
-    // 6TH RANK //
-    [ null, false ],                    // notation: a6     row: 2     column: 0
-    [ null, false ],                    // notation: b6     row: 2     column: 1
-    [ null, false ],                    // notation: c6     row: 2     column: 2
-    [ null, false ],                    // notation: d6     row: 2     column: 3
-    [ null, false ],                    // notation: e6     row: 2     column: 4
-    [ null, false ],                    // notation: f6     row: 2     column: 5
-    [ null, false ],                    // notation: g6     row: 2     column: 6
-    [ null, false ],                    // notation: h6     row: 2     column: 7
-
-    // 5TH RANK //
-    [ null, false ],                    // notation: a5     row: 3     column: 0
-    [ null, false ],                    // notation: b5     row: 3     column: 1
-    [ null, false ],                    // notation: c5     row: 3     column: 2
-    [ null, false ],                    // notation: d5     row: 3     column: 3
-    [ null, false ],                    // notation: e5     row: 3     column: 4
-    [ null, false ],                    // notation: f5     row: 3     column: 5
-    [ null, false ],                    // notation: g5     row: 3     column: 6
-    [ null, false ],                    // notation: h5     row: 3     column: 7
-
-    // 4TH RANK //
-    [ null, false ],                    // notation: a4     row: 4     column: 0
-    [ null, false ],                    // notation: b4     row: 4     column: 1
-    [ null, false ],                    // notation: c4     row: 4     column: 2
-    [ null, false ],                    // notation: d4     row: 4     column: 3
-    [ null, false ],                    // notation: e4     row: 4     column: 4
-    [ null, false ],                    // notation: f4     row: 4     column: 5
-    [ null, false ],                    // notation: g4     row: 4     column: 6
-    [ null, false ],                    // notation: h4     row: 4     column: 7
-
-    // 3RD RANK //
-    [ null, false ],                    // notation: a3     row: 5     column: 0
-    [ null, false ],                    // notation: b3     row: 5     column: 1
-    [ null, false ],                    // notation: c3     row: 5     column: 2
-    [ null, false ],                    // notation: d3     row: 5     column: 3
-    [ null, false ],                    // notation: e3     row: 5     column: 4
-    [ null, false ],                    // notation: f3     row: 5     column: 5
-    [ null, false ],                    // notation: g3     row: 5     column: 6
-    [ null, false ],                    // notation: h3     row: 5     column: 7
-
-    // 2ND RANK //
-    [ 'white-pawn', false ],            // notation: a2     row: 6     column: 0
-    [ 'white-pawn', false ],            // notation: b2     row: 6     column: 1
-    [ 'white-pawn', false ],            // notation: c2     row: 6     column: 2
-    [ 'white-pawn', false ],            // notation: d2     row: 6     column: 3
-    [ 'white-pawn', false ],            // notation: e2     row: 6     column: 4
-    [ 'white-pawn', false ],            // notation: f2     row: 6     column: 5
-    [ 'white-pawn', false ],            // notation: g2     row: 6     column: 6
-    [ 'white-pawn', false ],            // notation: h2     row: 6     column: 7
-
-    // 1ST RANK //
-    [ 'white-rook', false ],            // notation: a1     row: 7     column: 0
-    [ 'white-knight', false ],          // notation: b1     row: 7     column: 1
-    [ 'white-bishop', false ],          // notation: c1     row: 7     column: 2
-    [ 'white-queen', false ],           // notation: d1     row: 7     column: 3
-    [ 'white-king', false ],            // notation: e1     row: 7     column: 4
-    [ 'white-bishop', false ],          // notation: f1     row: 7     column: 5
-    [ 'white-knight', false ],          // notation: g1     row: 7     column: 6
-    [ 'white-rook', false ]             // notation: h1     row: 7     column: 7
-]
+const pieces = new Array(64).fill().map(() => []);      // creates array that stores which pieces are on a board and if any square should be highlighed
+pieces.forEach(array => { array.push(null, false); });  // fills array with init data
 
 export default function Board(props) {
     const [selectedRow, setSelectedRow] = useState(null);           // stores row of previously clicked square
@@ -103,11 +26,81 @@ export default function Board(props) {
     startIndex = props.isWhitesPOV ? 0 : 7;                         // sets start index based on which POV it should show
     endIndex = props.isWhitesPOV ? 8 : -1;                          // sets end index based on which POV it should show
 
+    // inports FEN //
+    const importFEN = (fen) => {
+        fen = fen.substr(0, fen.indexOf(' '))                           // only for first segment
+                               .replaceAll('8', '        ')             // replaces all '8's with 8 spaces
+                               .replaceAll('7', '       ')              // replaces all '7's with 7 spaces
+                               .replaceAll('6', '      ')               // replaces all '6's with 6 spaces
+                               .replaceAll('5', '     ')                // replaces all '5's with 5 spaces
+                               .replaceAll('4', '    ')                 // replaces all '4's with 4 spaces
+                               .replaceAll('3', '   ')                  // replaces all '3's with 3 spaces
+                               .replaceAll('2', '  ')                   // replaces all '2's with 2 spaces
+                               .replaceAll('1', ' ')                    // replaces all '1's with 1 space
+                               .replaceAll('/', '')                     // removes all '/'
+            + fen.substr(fen.indexOf(' '), fen.length);                 // gives remaining segments back to FEN
+        for (let i = 0; i < 64; i++) {                                  // for each square
+            pieces[i][0] = pieceLetterToWord(fen[i]);                   // convert format from fen to pieces
+        }
+        isWhitesMove = fen.substr(65, 1) === 'w';                       // sets isWhitesMove from FEN
+        if (!fen.substr(67, 1).includes('K')) whiteARookMoved = true;   // sets legality of white's short castle from FEN
+        if (!fen.substr(67, 2).includes('Q')) whiteHRookMoved = true;   // sets legality of white's long castle from FEN
+        if (!fen.substr(67, 3).includes('k')) blackARookMoved = true;   // sets legality of black's short castle from FEN
+        if (!fen.substr(67, 4).includes('q')) blackHRookMoved = true;   // sets legality of black's long castle from FEN
+        if (!fen.substr(68, 6).includes('-')) pieces[rowAndColumnToPosition(8 - fen.substr(fen.substr(68, 6).indexOf(' ') + 70, 1), fen.substr(fen.substr(68, 6).indexOf(' ') + 69, 1).charCodeAt(0) - 97)][0] = 'en-passant-square'; // sets en passant square from FEN
+        halfmoveClock = parseInt(fen.substr(0, fen.lastIndexOf(' ')).substr(fen.substr(0, fen.lastIndexOf(' ')).lastIndexOf(' ') + 1, fen.substr(0, fen.lastIndexOf(' ')).length));                                                   // sets halfmoveClock from FEN
+        fullmoveCounter = parseInt(fen.substr(fen.lastIndexOf(' '), fen.length));   // sets fullmoveCounter from FEN
+    }
+
+    // exports FEN //
+    const exportFEN = () => {
+        let fen = '';                                   // variable that stores FEN
+        for (let i = 0; i < 64; i++) {                  // for each square
+            if (pieces[i][0] === 'en-passant-square') fen += pieceWordToLetter(null) + fen.substr(i + 1);
+            else fen += pieceWordToLetter(pieces[i][0]) + fen.substr(i + 1);        // convert format from pieces to fen
+        }
+        for (let i = 0; i < 63; i = i + 9) {
+            fen = fen.substring(0, i + 8) + '/' + fen.substring(i + 8, fen.length); // adds slash after every 8 characters
+        }
+        fen = fen.replaceAll('        ', '8')           // replaces 8 spaces with '8'
+                 .replaceAll('       ', '7')            // replaces 7 spaces with '7'
+                 .replaceAll('      ', '6')             // replaces 6 spaces with '6'
+                 .replaceAll('     ', '5')              // replaces 5 spaces with '5'
+                 .replaceAll('    ', '4')               // replaces 4 spaces with '4'
+                 .replaceAll('   ', '3')                // replaces 3 spaces with '3'
+                 .replaceAll('  ', '2')                 // replaces 2 spaces with '2'
+                 .replaceAll(' ', '1')                  // replaces 1 space with '1'
+        fen += (isWhitesMove ? ' w' : ' b');            // if it is whites move, adds 'w' to FEN, if not, adds 'b' to FEN
+        {
+            let possibleCastles = '';                                       // stores possible castles
+            if (!whiteKingMoved) {                                          // if white's king did not move
+                if (!whiteARookMoved) possibleCastles += 'K';               // if white's a-rook did not move, adds this castle to variable
+                if (!whiteHRookMoved) possibleCastles += 'Q';               // if white's h-rook did not move, adds this castle to variable
+            }
+            if (!blackKingMoved) {                                          // if blacks's king did not move
+                if (!blackARookMoved) possibleCastles += 'k';               // if blacks's a-rook did not move, adds this castle to variable
+                if (!blackHRookMoved) possibleCastles += 'q';               // if blacks's h-rook did not move, adds this castle to variable
+            }
+            fen += ' ' + (possibleCastles !== '' ? possibleCastles : '-');  // adds castles to FEN, if there are not any possible, adds '-'
+        }
+        enPassantSection: {
+            for (let i = 0; i < 64; i++) {                                                                  // for each square
+                if (pieces[i][0] === 'en-passant-square') {                                                 // if it is en passant square
+                    fen += ' ' + String.fromCharCode(positionToColumn(i) + 97) + (8 - positionToRow(i));    // convert square number to square notation
+                    break enPassantSection;                                                                 // exists section
+                }
+            }
+            fen += ' -';            // if went through whole board and there is not any en passant square, adds '-' to FEN
+        }
+        fen += ' ' + halfmoveClock + ' ' + fullmoveCounter;     // adds halfmove clock and fullmove counter to FEN
+        return fen;                                             // returns FEN
+    }
+
     // sets legal moves //
     const setLegalMoves = (selectedSquare) => {
         let legalMoves = getLegalMoves(selectedSquare);
         for (let i = 0; i < 64; i++) {      // for each square on board
-            if (legalMoves[i])              // JUST PLACEHOLDER FOR NOW, SHOULD BE CHANGED TO CALL OF getLegalMoves() WHICH WILL BE COMPARED WITH INDEX
+            if (legalMoves[i])              // if it is legal move
                 pieces[i][1] = true;        // sets move to legal     
         }
     }
@@ -153,8 +146,12 @@ export default function Board(props) {
             setSelectedColumn(null);                                                                // clears previously selected column
             highlightedSquare = null;                                                               // sets highlighted square to none
         } else {                                                                                    // if move is valid
-            resetLegalMoves();                                                                      // resets legal moves     
-            isWhitesMove = !isWhitesMove;                                                           // flips turn white <---> black
+            halfmoveClock++;                                                                                                                // increments halfmoveClock
+            resetLegalMoves();                                                                                                              // resets legal moves     
+            if(!isWhitesMove) fullmoveCounter++;                                                                                            // increments fullmoveCounter after blacks move
+            isWhitesMove = !isWhitesMove;                                                                                                   // flips turn white <---> black
+            if(pieces[rowAndColumnToPosition(selectedRow, selectedColumn)][0].includes('pawn') ||                                           // if pawn moves
+               pieces[rowAndColumnToPosition(senderRow, senderColumn)][0] !== null) halfmoveClock = 0;                                      // or if anything is captures, it resets halfmoveClock
             if (pieces[rowAndColumnToPosition(selectedRow, selectedColumn)][0] === 'white-rook') {                                          // if white rook moved
                 if (rowAndColumnToPosition(selectedRow, selectedColumn) === 56) whiteARookMoved = true;                                     // if white a rook moved, it is rememberd in variable
                 else if (rowAndColumnToPosition(selectedRow, selectedColumn) === 63) whiteHRookMoved = true;                                // if white h rook moved, it is rememberd in variable
@@ -206,6 +203,7 @@ export default function Board(props) {
         pieces[props.sender][0] = props.promotedTo;     // places this piece on the board
         props.received();                               // sends back message that it was successfull
     }
+    if (JSON.stringify(pieces) === JSON.stringify(new Array(64).fill([ null, false ]))) importFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');  // on init imports starting position
 
     // renders 1 square //
     const renderSquare = (piecesIndex) => {
