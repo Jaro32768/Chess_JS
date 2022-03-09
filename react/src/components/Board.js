f8f3a54e21d40c21ba12f901fd311f1751b5077e
Jaro <jarko04@hotmail.sk>
2022-03-09 11:09:28 +0100

    Remove forgotten console.log()

--- react/src/components/Board.js
+++ react/src/components/Board.js
@@ -108,7 +108,6 @@ export default function Board(props) {
                 if (getLegalMoves(i, board)[kingsPosition]) return false;
             }
         }
-        console.log("true")
         return true;
     }
 
