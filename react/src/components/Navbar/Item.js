7b290fc20d36df9b563952c4d96695a140e2ddd1
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2022-05-19 10:58:11 +0200

    Revert "Revert "FEN implementation (WIP)""
    This reverts commit 4d50b38829b0fe746c766cf16c62e5982764b17d.

--- react/src/components/Navbar/Item.js
+++ react/src/components/Navbar/Item.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-export default function Item() {
+export default function Item(props) {
+
+  const handleClick = () => {
+    switch (props.type) {
+      case 'FEN':
+        handleFENClick();
+        break;
+    }
+  }
+
+  const handleFENClick = () => {
+    let test = prompt("test", props.getFEN());
+  }
+
   return (
-    <div className='navbarItem'>
-      ?
+    <div className='navbarItem' onClick={handleClick}>
+      {props.type}
     </div>
   )
 }
